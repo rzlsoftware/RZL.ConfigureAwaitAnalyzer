38fa0688d2236c88db553b0c36efe0d5cdfa0660
Jakob Wegenschimmel <jwegenschimmel@rzl.at>
2018-06-28 15:31:28 +0200

    ConfigureAwait(true) Analyzer

--- ConfigureAwaitAnalyzer/ConfigureAwaitAnalyzer/ConfigureAwaitAnalyzerAnalyzer.cs
+++ ConfigureAwaitAnalyzer/ConfigureAwaitAnalyzer/ConfigureAwaitAnalyzerAnalyzer.cs
@@ -1,11 +1,10 @@
-using System;
-using System.Collections.Immutable;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace ConfigureAwaitAnalyzer
 {
@@ -14,13 +13,16 @@ namespace ConfigureAwaitAnalyzer
     {
         public const string DiagnosticId = "ConfigureAwaitAnalyzer";
 
-        private static readonly LocalizableString MissingConfigureTitle = new LocalizableResourceString(nameof(Resources.MissingConfigureTitle), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString MissingConfigureAwaitTitle = new LocalizableResourceString(nameof(Resources.MissingConfigureTitle), Resources.ResourceManager, typeof(Resources));
         private static readonly LocalizableString MissingConfigureAwaitMessage = new LocalizableResourceString(nameof(Resources.MissingConfigureAwaitMessage), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString ConfigureAwaitTrueTitle = new LocalizableResourceString(nameof(Resources.ConfigureAwaitTrueTitle), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString ConfigureAwaitTrueMessage = new LocalizableResourceString(nameof(Resources.ConfigureAwaitTrueMessage), Resources.ResourceManager, typeof(Resources));
         private const string Category = "ConfigureAwaitAnalyzer";
 
-        private readonly static DiagnosticDescriptor MissingConfigureAwaitRule = new DiagnosticDescriptor(DiagnosticId, MissingConfigureTitle, MissingConfigureAwaitMessage, Category, DiagnosticSeverity.Error, isEnabledByDefault: true, customTags: "missing");
+        private readonly static DiagnosticDescriptor MissingConfigureAwaitRule = new DiagnosticDescriptor(DiagnosticId, MissingConfigureAwaitTitle, MissingConfigureAwaitMessage, Category, DiagnosticSeverity.Error, isEnabledByDefault: true, customTags: "missing");
+        private readonly static DiagnosticDescriptor ConfigureAwaitTrueRule = new DiagnosticDescriptor(DiagnosticId, ConfigureAwaitTrueTitle, ConfigureAwaitTrueMessage, Category, DiagnosticSeverity.Error, isEnabledByDefault: true, customTags: "true");
 
-        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(MissingConfigureAwaitRule);
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(MissingConfigureAwaitRule, ConfigureAwaitTrueRule);
 
         public override void Initialize(AnalysisContext context)
             => context.RegisterSyntaxNodeAction(AnalyzeNode, SyntaxKind.AwaitExpression);
@@ -37,6 +39,21 @@ namespace ConfigureAwaitAnalyzer
             if (!containsConfigureAwait)
             {
                 context.ReportDiagnostic(Diagnostic.Create(MissingConfigureAwaitRule, awaitExpression.GetLocation()));
+                return;
+            }
+
+            var isConfigureAwaitTrue = awaitExpression
+                .DescendantNodes()
+                .OfType<ArgumentListSyntax>()
+                .Last()
+                .Arguments
+                .First()
+                .GetFirstToken()
+                .IsKind(SyntaxKind.TrueKeyword);
+
+            if (isConfigureAwaitTrue)
+            {
+                context.ReportDiagnostic(Diagnostic.Create(ConfigureAwaitTrueRule, awaitExpression.GetLocation()));
                 return;
             }
         }
