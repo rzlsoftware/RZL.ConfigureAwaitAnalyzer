92adda942c6133dfa157e29370008a59939723eb
Jakob Wegenschimmel <jwegenschimmel@rzl.at>
2018-06-28 13:17:24 +0200

    ConfigureAwaitAnalyzerAnalyzer cleanup and rename fields and properties

--- ConfigureAwaitAnalyzer/ConfigureAwaitAnalyzer/ConfigureAwaitAnalyzerAnalyzer.cs
+++ ConfigureAwaitAnalyzer/ConfigureAwaitAnalyzer/ConfigureAwaitAnalyzerAnalyzer.cs
@@ -1,11 +1,7 @@
 using System;
-using System.Collections.Generic;
 using System.Collections.Immutable;
-using System.Linq;
-using System.Threading;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
 
 namespace ConfigureAwaitAnalyzer
@@ -15,37 +11,20 @@ namespace ConfigureAwaitAnalyzer
     {
         public const string DiagnosticId = "ConfigureAwaitAnalyzer";
 
-        // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
-        // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Localizing%20Analyzers.md for more on localization
-        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
-        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
-        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
-        private const string Category = "Naming";
+        private static readonly LocalizableString MissingConfigureTitle = new LocalizableResourceString(nameof(Resources.MissingConfigureTitle), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString MissingConfigureAwaitMessageFormat = new LocalizableResourceString(nameof(Resources.MissingConfigureAwaitMessageFormat), Resources.ResourceManager, typeof(Resources));
+        private const string Category = "ConfigureAwaitAnalyzer";
 
-        private static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
+        private static DiagnosticDescriptor MissingConfigureAwaitRule = new DiagnosticDescriptor(DiagnosticId, MissingConfigureTitle, MissingConfigureAwaitMessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, customTags: "MissingConfigureAwait");
 
-        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(MissingConfigureAwaitRule);
 
         public override void Initialize(AnalysisContext context)
+            => context.RegisterSyntaxNodeAction(AnalyzeNode, SyntaxKind.AwaitExpression);
+
+        private void AnalyzeNode(SyntaxNodeAnalysisContext obj)
         {
-            // TODO: Consider registering other actions that act on syntax instead of or in addition to symbols
-            // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Analyzer%20Actions%20Semantics.md for more information
-            context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.NamedType);
-        }
-
-        private static void AnalyzeSymbol(SymbolAnalysisContext context)
-        {
-            // TODO: Replace the following code with your own analysis, generating Diagnostic objects for any issues you find
-            var namedTypeSymbol = (INamedTypeSymbol)context.Symbol;
-
-            // Find just those named type symbols with names containing lowercase letters.
-            if (namedTypeSymbol.Name.ToCharArray().Any(char.IsLower))
-            {
-                // For all such symbols, produce a diagnostic.
-                var diagnostic = Diagnostic.Create(Rule, namedTypeSymbol.Locations[0], namedTypeSymbol.Name);
-
-                context.ReportDiagnostic(diagnostic);
-            }
+            throw new NotImplementedException();
         }
     }
 }
